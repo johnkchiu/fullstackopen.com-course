221562830dc4893685afa40eb4ccf6174b46ba97
John Chiu <johnkchiu@gmail.com>
2023-11-13 23:24:55 -0800

    Completed 3.6: Phonebook backend step6

--- phonebook/src/server/index.cjs
+++ phonebook/src/server/index.cjs
@@ -63,9 +63,17 @@ app.delete("/api/persons/:id", (request, response) => {
 app.post("/api/persons", (request, response) => {
   const body = request.body;
 
+  // check for name and number values.
   if (!body.name || !body.number) {
     return response.status(400).json({
-      error: "content name or number",
+      error: "The name or number is missing",
+    });
+  }
+
+  // check if name already exist
+  if (persons.find((p) => p.name === body.name)) {
+    return response.status(400).json({
+      error: "The name already exists in the phonebook"
     });
   }
 
